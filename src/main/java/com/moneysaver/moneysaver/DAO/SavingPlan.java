a0593609b7206510091fad219a6703eed619f734
Daoudi Zakaria <mr.zakariadaoudi@gmail.com>
2021-06-04 17:15:29 +0100

    mapping hibernate

---
+++ src/main/java/com/moneysaver/moneysaver/DAO/SavingPlan.java
@@ -0,0 +1,25 @@
+package com.moneysaver.moneysaver.DAO;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+public class SavingPlan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idSavingPlan ;
+    private String savingPlanName ;
+    private Float budget ;
+    private Date dateOfStart ;
+    private Date dateOfEnd ;
+    private Float savingGoal ;
+
+    @ManyToOne
+    @JoinColumn(name = "idUser")
+    private User user;
+
+
+}
